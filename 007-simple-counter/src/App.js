6187cc53138ecde7adb0927969e73b8cf2656cbb
xomrau <hle3524@gmail.com>
2020-06-30 17:31:05 +1000

    added Counter class

--- 007-simple-counter/src/App.js
+++ 007-simple-counter/src/App.js
@@ -1,26 +1,43 @@
 import React from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      counter: 0
+    };
+    this.increment = this.increment.bind(this);
+    this.decrement = this.decrement.bind(this);
+    this.reset = this.reset.bind(this);
+  }
+  increment() {
+    this.setState((state) => ({
+      counter: state.counter + 1
+    }));
+  }
+  decrement() {
+    this.setState((state) => ({
+      counter: state.counter - 1
+    }));
+  }
+  reset() {
+    this.setState(() => ({
+      counter: 0
+    }));
+  }
 
-export default App;
+  render(){
+    return(
+      <div>
+        <div className="buttons-group">
+          <button onClick={this.increment}>Increment</button>
+          <button onClick={this.decrement}>Decrement</button>
+          <button onClick={this.reset}>Reset</button>
+        </div>
+        <p>Counter: {this.state.counter}</p>
+      </div>
+    );
+  }
+}
