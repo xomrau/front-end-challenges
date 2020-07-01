e4f36860a029cff3de64b3b427d828853ac3dfd7
xomrau <hle3524@gmail.com>
2020-07-01 14:52:15 +1000

    .

--- 008-facebook-comment-card/src/App.js
+++ 008-facebook-comment-card/src/App.js
@@ -15,7 +15,7 @@ export default class App extends React.Component {
         <Image />
         <User />
         <Description />
-        <LikeReply />
+        <Interactives />
       </div>
     );
   }
